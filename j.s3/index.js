56e10ad2dbee74d88676dcfa43e7af6861f597ce
Farzan khan <farzaan341@gmail.com>
2026-07-16 17:30:37 +0500

    Add files via upload
    Chapter 03, 04 & 05 parctice of JS

---
+++ j.s3/index.js
@@ -0,0 +1,35 @@
+//task1//
+let name="19";
+let mark="74";
+let salary="25,0000,000,00";
+let year="2026";
+let price ="12Rs";
+console.log(name, mark, salary, year,price);
+
+//task2//
+
+let _age;
+let $price;
+let student1;
+
+/*let 1name;     
+let user-name;
+let first name;*/
+
+
+//task3
+let a = 100;
+let b = 20;
+
+console.log("Sum =", a + b);
+
+console.log("Difference =", a - b);
+
+console.log("Product =", a * b);
+
+console.log("Division =", a / b);
+
+
+console.log("Remainder =", a % b);
+
+
